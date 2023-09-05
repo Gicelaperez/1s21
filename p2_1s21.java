d20c65eb6154463e8478f0bc51304b5572af228a
Gicelaperez <144179586+Gicelaperez@users.noreply.github.com>
2023-09-05 16:11:47 -0600

    Add files via upload
    practica 1 y 2 del dia 5 de septiembre del 2023

---
+++ p2_1s21.java
@@ -0,0 +1,10 @@
+class p2_1s21{
+	public static void main(String[] args) {
+		System.out.println("Datos personales");
+		System.out.println("Myriam Gicela");
+		System.out.println("18");
+		System.out.println("myriamgicela3@gmail.com");
+		System.out.println("futbol");
+		System.out.println("blanco");
+	}
+}
